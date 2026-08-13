de9e21b93fd07cb174d3008a0fd45e754c443bdf
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-08-13 14:47:37 -0300

    add as rotas entre os arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { CadastroAtleta } from './cadastro-atleta/cadastro-atleta';
+import { CadastroCorrida } from './cadastro-corrida/cadastro-corrida';
+import { CorridasDisponiveis } from './corridas-disponiveis/corridas-disponiveis';
+import { InscricaoCorrida } from './inscricao-corrida/inscricao-corrida';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'cadastro-atleta',
+        pathMatch: 'full'
+      },
+    
+      {
+        path: 'cadastro-atleta',
+        component: CadastroAtleta
+      },
+    
+      {
+        path: 'cadastro-corrida',
+        component: CadastroCorrida
+      },
+
+      {
+        path: 'corrida-disponivel',
+        component: CorridasDisponiveis
+      },
+
+      {
+        path: 'inscricao-corrida',
+        component: InscricaoCorrida
+      }
+    
+];
+
+
+
